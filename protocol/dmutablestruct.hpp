608e9878a7f42b357db3b118e2151923a7dbc2a0
sja <sja@arxsys.fr>
2015-02-26 18:17:53 +0100

    add export to dmutablestruct.hpp

--- protocol/dmutablestruct.hpp
+++ protocol/dmutablestruct.hpp
@@ -1,6 +1,7 @@
 #ifndef DESTRUCT_DMUTABLE_STRUCT_HPP_
 #define DESTRUCT_DMUTABLE_STRUCT_HPP_
 
+#include "destruct.hpp"
 #include "dstruct.hpp"
 
 namespace Destruct
@@ -13,15 +14,15 @@ public:
   typedef DAttributeContainer::const_iterator DAttributeIterator;
 
   template<typename Iterator> 
-  DMutableStruct(DStruct const* base, const DUnicodeString& name, CreateMutableObjectFunction newObj, Iterator attributeBegin, Iterator attributeEnd) : DStruct(base, name, (DObject* (*) (DStruct*, DValue const&))newObj, attributeBegin, attributeEnd), __createObject(newObj)
+  EXPORT DMutableStruct(DStruct const* base, const DUnicodeString& name, CreateMutableObjectFunction newObj, Iterator attributeBegin, Iterator attributeEnd) : DStruct(base, name, (DObject* (*) (DStruct*, DValue const&))newObj, attributeBegin, attributeEnd), __createObject(newObj)
   {
   }
-  DMutableStruct(DStruct const* base, const DUnicodeString& name, CreateMutableObjectFunction objectFunction);
-  ~DMutableStruct();
+  EXPORT DMutableStruct(DStruct const* base, const DUnicodeString& name, CreateMutableObjectFunction objectFunction);
+  EXPORT ~DMutableStruct();
 
-  DObject* newObject(DValue const& args);
-  void     addAttribute(const DAttribute& attribute);
-  void     replaceAttribute(size_t idx,const DAttribute& attribute);
+  EXPORT DObject* newObject(DValue const& args);
+  EXPORT void     addAttribute(const DAttribute& attribute);
+  EXPORT void     replaceAttribute(size_t idx,const DAttribute& attribute);
 private:
   const CreateMutableObjectFunction    __createObject;
 };
