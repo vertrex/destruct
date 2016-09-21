400bb1cf40a750b5d528a140216c862907b11580
sja <sja@digital-forensic.org>
2016-09-21 15:20:18 +0200

    cmakelist remove profiling

--- python/examples/device/device.py
+++ python/examples/device/device.py
@@ -13,8 +13,8 @@ loader.file("../../../examples/modules/libdestruct_device.so")
 print 'ok loaded'
 arg = DStructs().find("ClientArgument").newObject()
 #arg.address = "127.0.0.1"
-arg.address = "127.0.0.1"
-#arg.address = "10.42.1.197"
+#arg.address = "127.0.0.1"
+arg.address = "10.42.1.197"
 #arg.address = "10.42.1.195"
 arg.port = 0xdff
 client = DStructs().find("Client").newObject(arg)
